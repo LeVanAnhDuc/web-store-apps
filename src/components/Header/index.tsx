dbfbf235455e21777070d2f21a09f18b7f7dfa8e
LrVanAnhDuc <levananhduc1804@gmail.com>
2026-03-15 13:21:41 +0700

    chore: rename project from Apartment/Platform Learning to App Store

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,7 @@ const Header = () => (
   <header className="fixed top-0 right-0 left-0 z-50 flex h-16 w-full items-center justify-between bg-transparent px-6 py-3">
     <section className="flex items-center gap-2">
       <Logo />
-      <span className="text-foreground font-semibold">Platform Learning</span>
+      <span className="text-foreground font-semibold">App Store</span>
     </section>
 
     <section className="flex items-center justify-center gap-2">
